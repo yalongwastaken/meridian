b0b905266b9ee0cefb242ca235aca6e60bad3ee1
yalongwastaken <yalong.anthony123@gmail.com>
2026-05-20 08:34:30 -0700

    esp32: scaffold component structure for uart_rx, mqtt_pub, and sleep

---
+++ esp32/main/main.c
@@ -0,0 +1,16 @@
+/**
+ * @file main.c
+ * @brief TODO
+ * 
+ * @lastmodified 5/20/2026
+ * @version 0.0
+ */
+
+#include "mqtt_pub.h"
+#include "sleep.h"
+#include "uart_rx.h"
+#include "freertos/FreeRTOS.h"
+
+void app_main(void) {
+    // TODO
+}
